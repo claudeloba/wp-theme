11d875c708d969e8c2113901c12132444ee466fe
Claude-Marcel Loba <100008570+claudeloba@users.noreply.github.com>
2023-09-04 12:58:44 +0200

    delade upp footer och header ifrån front-page

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
+// Lägger till dynamic tag
+add_theme_support('title_tag');
+
 function register_styles() {
     $version = wp_get_theme()->get( 'Version' );
     wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.1.0', 'all');
@@ -10,7 +13,7 @@ add_action('wp_enqueue_scripts', 'register_styles');
 
 function register_scripts() {
     wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js', array(), '5.2.2', true);
-    wp_enqueue_script('main-js', get_template_directory_uri(), '/assets/javascript/bold-and-bright.js', array(), '1', true);
+    wp_enqueue_script('main-js', get_template_directory_uri().'/assets/javascript/script.js', array(), '1', true);
     
 }
 
